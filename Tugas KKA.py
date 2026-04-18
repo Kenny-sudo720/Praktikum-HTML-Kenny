dde36291f9c89f516b2207556338b0945c940510
Takeo Häyhä <kekenny720@gmail.com>
2026-04-18 08:28:17 +0700

    Add files via upload

---
+++ Tugas KKA.py
@@ -0,0 +1,25 @@
+Nilai_Siswa = [85, 90, 78, 92, 88]
+
+Nilai_Siswa.append(95)
+
+print("Nilai Siswa:", Nilai_Siswa)
+
+
+Koordinat = (10.5, 20.3)
+
+print("Koordinat:", Koordinat)
+
+
+Siswa = {
+    "Nama": "Ani",
+    "Kelas": "10-A",
+    "Nilai": 88
+}
+
+print("Nama siswa:", Siswa["Nama"])
+print("Kelas siswa:", Siswa["Kelas"])
+
+
+Peserta = {"Budi", "Siti", "Andi", "Budi"}
+
+print("Peserta:", Peserta)
